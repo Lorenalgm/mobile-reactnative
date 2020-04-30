b0c21d0613bbf3bf4d0b7e27b5f328e9ac6a298d
Lorenalgm <lorena@proesc.com>
2020-04-30 14:06:09 -0300

    Adicionada listagem de projetos da api

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, StatusBar, FlatList } from 'react-native';
+import api from './services/api';
+
+export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <SafeAreaView style={styles.container}>
+            <FlatList 
+                data={projects}
+                keyExtractor={project => project.id}
+                renderItem={({item: project}) => (
+                    <Text key={project.id} style={styles.project}>{project.title}</Text>
+                    )}
+            />
+            </SafeAreaView>
+        </>
+    ) 
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+    },
+    project: {
+        color: '#FFF',
+        fontSize: 30,
+    }
+})
